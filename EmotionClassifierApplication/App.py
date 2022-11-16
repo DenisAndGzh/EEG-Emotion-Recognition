9f83946487eb8a83bf782ca6f3e27ff0f43a9567
GG <zjjhgzh@gmail.com>
2022-11-16 17:56:29 +0300

    format

--- EmotionClassifierApplication/App.py
+++ EmotionClassifierApplication/App.py
@@ -9,11 +9,11 @@ sys.path.append("..")
 import EmotionClassifierModel.Models
 
 window = tk.Tk()
-window.title('Emotion Recognition Application')
-window.geometry('650x450+400+300')
+window.title("Emotion Recognition Application")
+window.geometry("650x450+400+300")
 window.resizable(0, 0)
 window.attributes("-alpha", 0.9)
-window.iconbitmap('imgs/Brain-icon.ico')
+window.iconbitmap("imgs/Brain-icon.ico")
 
 f = tk.StringVar()
 
@@ -22,42 +22,49 @@ lable_dict = {-1: "negative", 0: "neutral", 1: "positive"}
 
 
 def upload():
-    f.set(filedialog.askopenfilename(title='OpenBCI output files',
-                                     filetypes=[('csv', '*.csv'), ('All files', '*')]))
-    
+    f.set(
+        filedialog.askopenfilename(
+            title="OpenBCI output files",
+            filetypes=[("csv", "*.csv"), ("All files", "*")],
+        )
+    )
+
+
 def predict():
     if len(f.get()) == 0:
-        messagebox.showinfo(title='Warning', message='You haven\'t upload file!')
+        messagebox.showinfo(title="Warning", message="You haven't upload file!")
         return
     EC = EmotionClassifierModel.Models.EmotionClassifier(True, f)
     EC.Init_train_test_data()
     match cbox.get():
         case "SVM":
             EC.SVM_model()
-        case 'AdaBoost':
+        case "AdaBoost":
             EC.AdaBoost_model()
-        case 'MLP':
+        case "MLP":
             EC.MLP_model()
     result = EC.get_predicted_value()
     img = ImageTk.PhotoImage(Image.open(img_dict[result]))
     label_img = tk.Label(image=img)
     label_img.image = img
     label_img.place(anchor="center", x=325, y=135)
-    tk.Label(window, text=lable_dict[result], font=(
-        "微软雅黑", 25)).place(anchor="center", x=325, y=280)
-    
+    tk.Label(window, text=lable_dict[result], font=("微软雅黑", 25)).place(
+        anchor="center", x=325, y=280
+    )
+
 
 cbox = ttk.Combobox(window)
-cbox['value'] = ('SVM', 'AdaBoost', 'MLP')
+cbox["value"] = ("SVM", "AdaBoost", "MLP")
 cbox.current(0)
-cbox.grid(row = 1, sticky="NW")
+cbox.grid(row=1, sticky="NW")
 
-tk.Label(window, textvariable=f, font=("微软雅黑", 13)
-         ).place(anchor="center", x=325, y=330)
-tk.Button(window, text="Upload and predict", width=15, height=3,
-          command=upload).place(anchor="center", x=163, y=385)
-tk.Button(window, text="Upload and predict", width=15, height=3,
-          command=predict).place(anchor="center", x=487, y=385)
+tk.Label(window, textvariable=f, font=("微软雅黑", 13)).place(anchor="center", x=325, y=330)
+tk.Button(window, text="Upload and predict", width=15, height=3, command=upload).place(
+    anchor="center", x=163, y=385
+)
+tk.Button(window, text="Upload and predict", width=15, height=3, command=predict).place(
+    anchor="center", x=487, y=385
+)
 
 
 window.mainloop()
